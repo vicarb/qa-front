e729fd57ab10c6350b7f1114aa454051e7398e03
vicarb <vcardenasb@protonmail.com>
2023-07-10 20:51:14 -0400

    added filtering

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react';
+
 interface Item {
   id: number;
   question: string;
@@ -8,6 +9,8 @@ interface Item {
 
 function Landing() {
   const [data, setData] = useState<Item[] | null>(null);
+  const [filteredData, setFilteredData] = useState<Item[] | null>(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     async function fetchData() {
@@ -15,18 +18,42 @@ function Landing() {
       // const res = await fetch('https://my-cloudrun-service-c2jkmf2kea-uw.a.run.app/api/question-answers');
       const json = await res.json();
       setData(json);
+      setFilteredData(json);
     }
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      const filteredItems = data.filter((item) =>
+        item.question.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredData(filteredItems);
+    }
+  }, [data, searchTerm]);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
     <div className="bg-gray-100 min-h-screen py-8">
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold mb-8 text-green-500 has text-center">Interview Questions</h1>
 
-        {data ? (
+        <div className="mb-4">
+          <input
+            type="text"
+            className="border border-gray-300 rounded py-2 px-4 w-full"
+            placeholder="Search by question"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+        </div>
+
+        {filteredData ? (
           <div className="grid gap-8 lg:grid-cols-2">
-            {data.map((item) => (
+            {filteredData.map((item) => (
               <div key={item.id} className="bg-white shadow rounded-lg">
                 <div className="p-6">
                   <h3 className="text-xl font-semibold mb-4">{item.question}</h3>
