08ed77d504fb95dc5f47bb2c29bce7d75035a81e
vicarb <vcardenasb@protonmail.com>
2023-07-11 22:59:15 -0400

    added loading spinner

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -93,7 +93,9 @@ function Landing() {
             ))}
           </div>
         ) : (
-          <p className="text-center">Loading...</p>
+          <div className="flex justify-center items-center h-64">
+    <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-gray-900"></div>
+  </div>
         )}
       </div>
     </div>
