57ecc6e833903481a26c51f0f34331b1cae2f638
vicarb <vcardenasb@protonmail.com>
2023-07-26 00:26:17 -0400

    updated landing fetch url

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -1,21 +1,32 @@
-import React from "react";
+'use client'
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
-const concepts = [
-  { id: 1, name: "Node.js", slug: "nodejs" },
-  { id: 2, name: "Java", slug: "java" },
-  { id: 3, name: "Object-Oriented Programming", slug: "oop" },
-];
-
 const LandingPage = () => {
+  const [concepts, setConcepts] = useState([]);
+
+  useEffect(() => {
+    // Fetch concepts (interview question categories) from your API endpoint
+    const fetchConcepts = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8082/api/data");
+        const data = await response.json();
+        setConcepts(data);
+      } catch (error) {
+        console.error("Error fetching concepts:", error);
+      }
+    };
+
+    fetchConcepts();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-600 to-blue-400 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto">
         <h1 className="text-4xl font-bold text-center text-white mb-8">Interview Questions</h1>
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           {concepts.map((concept) => (
-            <Link href={`/concepts/${concept.id}`} key={concept.id}>
-
+            <Link href={`/concepts/${concept.slug}`} key={concept.id}>
               <span className="flex items-center justify-center h-32 rounded-lg shadow-lg bg-white text-center transition-colors duration-300 hover:bg-blue-500 hover:text-white">
                 {concept.name}
               </span>
