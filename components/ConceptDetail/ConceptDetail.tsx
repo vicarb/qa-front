fa569dc0f23d9e637dec53a822e85030f3edf53c
vicarb <vcardenasb@protonmail.com>
2023-07-27 01:30:06 -0400

    updated dynamic filtering

--- components/ConceptDetail/ConceptDetail.tsx
+++ components/ConceptDetail/ConceptDetail.tsx
@@ -5,42 +5,44 @@ import axios from "axios";
 export const ConceptDetail = ({ params }) => {
   const { id } = params;
   const [concept, setConcept] = useState(null);
+  const [concepts, setConcepts] = useState([]);
   console.log("id", id);
   useEffect(() => {
-    if (id) {
-      fetchConcepts();
-    }
-  }, [id]);
+    fetchConcepts();
+  }, []);
 
   const fetchConcepts = async () => {
     try {
       const response = await axios.get("http://127.0.0.1:8082/api/data");
       const data = response.data;
 
-      // Filter the data based on the 'id' parameter
-      const filteredConcept = data.find((item) => item.category === id);
+      // Filter the data based on the 'category' provided by the 'id' parameter
+      const filteredConcepts = data.filter((item) => item.category === id);
 
-      if (filteredConcept) {
-        setConcept(filteredConcept);
+      if (filteredConcepts.length > 0) {
+        setConcepts(filteredConcepts);
       } else {
-        console.error("Concept not found with ID:", id);
+        console.error("No concepts found with Category:", id);
       }
     } catch (error) {
       console.error("Error fetching concepts:", error);
     }
   };
-  console.log(concept);
-  
-  
+
   return (
-    <div>ConceptDetail
-    {concept && (
-      <div>
-        <h1>{concept.category}</h1>
-        <h1>{concept.question}</h1>
-        <h1>{concept.answer}</h1>
-        </div>
-    )}
+    <div className="min-h-screen bg-gradient-to-b from-blue-600 to-blue-400 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md mx-auto">
+        {concepts.map((concept) => (
+          <div key={concept._id} className="bg-white shadow-lg rounded-lg p-6 mb-4">
+            <h1 className="text-3xl font-bold mb-4">{concept.category}</h1>
+            <p className="text-lg text-gray-800 mb-6">{concept.question}</p>
+            <p className="text-base text-gray-600">{concept.answer}</p>
+          </div>
+        ))}
+        {concepts.length === 0 && (
+          <p className="text-lg text-gray-800">No concepts found with Category: {id}</p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
