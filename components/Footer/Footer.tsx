7a805f948d6c94ed9811e2a6173478c31449461f
vicarb <vcardenasb@protonmail.com>
2023-08-10 22:56:36 -0400

    updated footer text

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="bg-blue-600 mt-0 py-8 text-center text-white">
-      <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Share your interview questions with us!</p>
     </footer>
   );
 };
