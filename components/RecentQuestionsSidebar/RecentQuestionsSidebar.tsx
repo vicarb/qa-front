ad499ff6ad0aee9e174df57dfa91fba283ef1027
vicarb <vcardenasb@protonmail.com>
2023-08-30 23:57:41 -0400

    implemented mobile sidebar

--- components/RecentQuestionsSidebar/RecentQuestionsSidebar.tsx
+++ components/RecentQuestionsSidebar/RecentQuestionsSidebar.tsx
@@ -4,7 +4,6 @@
 import React, { useState, useEffect } from 'react';
 
 const RecentQuestionsSidebar: React.FC = () => {
-  // Mock data for recent questions
   const mockRecentQuestions = [
     'What is Big O notation?',
     'Explain event delegation in JavaScript',
@@ -14,6 +13,7 @@ const RecentQuestionsSidebar: React.FC = () => {
   ];
 
   const [recentQuestions, setRecentQuestions] = useState<string[]>([]);
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
 
   useEffect(() => {
     // Simulate an API call with a 2-second delay
@@ -23,20 +23,42 @@ const RecentQuestionsSidebar: React.FC = () => {
   }, []);
 
   return (
-    <div className="fixed right-0 top-0 h-full bg-gray-100 w-64 p-4 shadow-lg overflow-y-auto">
-      <h3 className="text-lg font-semibold mb-4 border-b pb-2">Recently Added Questions</h3>
-      <ul className="space-y-2">
-        {recentQuestions.length === 0 ? (
-          <p className="text-gray-500">Loading...</p>
-        ) : (
-          recentQuestions.map((question, index) => (
-            <li key={index} className="bg-white p-2 rounded shadow hover:bg-gray-200 transition duration-200">
-              {question}
-            </li>
-          ))
-        )}
-      </ul>
-    </div>
+    <>
+    <button
+    className="lg:hidden fixed right-4 bottom-4 z-50 p-2 rounded-full shadow"
+    onClick={() => setShowSidebar(!showSidebar)}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4 6h16M4 12h16m-7 6h7"
+      />
+    </svg>
+  </button>
+  
+      <div className={`fixed right-0 top-0 h-full bg-gray-100 w-64 p-4 shadow-lg overflow-y-auto transform transition-transform duration-300 ${showSidebar || 'translate-x-64'} lg:translate-x-0`}>
+        <h3 className="text-lg font-semibold mb-4 border-b pb-2">Recently Added Questions</h3>
+        <ul className="space-y-2">
+          {recentQuestions.length === 0 ? (
+            <p className="text-gray-500">Loading...</p>
+          ) : (
+            recentQuestions.map((question, index) => (
+              <li key={index} className="bg-white p-2 rounded shadow hover:bg-gray-200 transition duration-200">
+                {question}
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    </>
   );
 };
 
