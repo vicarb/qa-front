4118a761f2e107551aac963e7c394f4fde1f2cc8
vicarb <vcardenasb@protonmail.com>
2023-09-01 01:20:49 -0400

    updated mobile navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -7,49 +7,41 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="bg-blue-500 p-4">
-      <div className="flex items-center justify-between flex-wrap">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <span className="font-semibold text-xl tracking-tight">Your App</span>
-        </div>
-        <div className="block lg:hidden">
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="flex items-center px-3 py-2 border rounded text-blue-200 border-blue-400 hover:text-white hover:border-white"
-          >
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? '' : 'hidden'}`}>
-          <div className="text-sm lg:flex-grow">
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4"
-            >
-              Home
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4"
-            >
-              About
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white"
-            >
-              Contact
-            </a>
-          </div>
-        </div>
+  <div className="flex items-center justify-between flex-wrap">
+    <div className="flex items-center flex-shrink-0 text-white mr-6">
+      <span className="font-semibold text-xl tracking-tight">Your App</span>
+    </div>
+    <div className="block lg:hidden">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="bg-blue-400 hover:bg-blue-500 text-white rounded-full p-2 shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-300 ease-in-out"
+      >
+        <span className="sr-only">Open menu</span>
+        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
+    </div>
+    <div
+      className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto transition-all duration-300 ease-in-out overflow-hidden ${
+        isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
+      } lg:max-h-full lg:opacity-100`}
+    >
+      <div className="text-sm lg:flex-grow">
+        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
+          Home
+        </a>
+        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
+          About
+        </a>
+        <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white">
+          Contact
+        </a>
       </div>
-    </nav>
+    </div>
+  </div>
+</nav>
+
   );
 };
 
