62abd242d0e43968df5bc577780a95b0eafa8cfe
vicarb <vcardenasb@protonmail.com>
2023-09-05 12:56:29 -0300

    changed landing page client side rendering to server side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,31 @@ import Landing from '@/components/Landing/Landing'
 import Image from 'next/image'
 import SecLanding from '@/components/SecLanding/SecLanding'
 import LandingPage from '@/components/LandingPage/LandingPage'
+
 import logger from '../lib/logger';
 import Footer from '@/components/Footer/Footer';
 import RecentQuestionsSidebar from '@/components/RecentQuestionsSidebar/RecentQuestionsSidebar';
 
-export default function Home() {
+async function getConceptsData() {
+  const res = await fetch("https://my-service5-52m34p25ra-uk.a.run.app/api/data");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch concepts data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getConceptsData();
   logger.debug('Rendering HomePage');
+
   
   return (
     <>
 
 
-    <LandingPage/>
+    <LandingPage data={data} />
     <RecentQuestionsSidebar/>
 
     </>  )
--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -1,50 +1,26 @@
-'use client'
+
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { Concept } from "@/interfaces/Concept/Concept";
 import Spinner from "../Spinner/Spinner";
 
-const LandingPage = () => {
-  const [concepts, setConcepts] = useState<string[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchConcepts = async () => {
-      try {
-        const response = await fetch("https://my-service5-52m34p25ra-uk.a.run.app/api/data");
-        const data: Concept[] = await response.json();
-        const uniqueCategories = new Set(data.map((concept) => concept.category));
-        setConcepts(Array.from(uniqueCategories));
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching concepts:", error);
-        setLoading(false);
-      }
-    };
-
-    fetchConcepts();
-  }, []);
+const LandingPage: React.FC<{ data: Concept[] }> = ({ data }) => {
+  const uniqueCategories = new Set(data.map((concept) => concept.category));
 
   return (
     <div className="h-[calc(100vh-4rem)] bg-gradient-to-b from-blue-600 to-blue-400 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto">
         <h1 className="text-4xl font-bold text-center text-white mb-8">Interview Questions</h1>
-        {loading ? (
-          // Center the Spinner component using flexbox properties
-          <div className="flex items-center justify-center">
-            <Spinner />
-          </div>
-        ) : (
-          <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-            {concepts.map((category) => (
-              <Link href={`/concept/${category}`} key={category}>
-                <span className="flex items-center justify-center text-slate-600 h-32 rounded-lg shadow-lg bg-white text-center transition-colors duration-300 font-semibold hover:bg-blue-500 hover:text-white text-lg">
-                  {category}
-                </span>
-              </Link>
-            ))}
-          </div>
-        )}
+        
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          {Array.from(uniqueCategories).map((category) => (
+            <Link href={`/concept/${category}`} key={category}>
+              <span className="flex items-center justify-center text-slate-600 h-32 rounded-lg shadow-lg bg-white text-center transition-colors duration-300 font-semibold hover:bg-blue-500 hover:text-white text-lg">
+                {category}
+              </span>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
