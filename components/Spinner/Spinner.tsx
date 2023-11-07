bf6bcdfca6e869a114c77e21ef9fe476ae4b9be4
vicarb <108682674+vicarb@users.noreply.github.com>
2023-11-06 22:04:59 -0300

    Update Spinner Color

--- components/Spinner/Spinner.tsx
+++ components/Spinner/Spinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center h-full">
-      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-green-500"></div>
     </div>
   );
 };
